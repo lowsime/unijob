49577601d44d313c2bad376edc331f013f7e8c70
Sime <87931694+lowsime@users.noreply.github.com>
2025-09-21 20:01:57 +0200

    Update s_webhook.lua
    Remove my webhook ;p

--- ls_unijob/server/s_webhook.lua
+++ ls_unijob/server/s_webhook.lua
@@ -1,14 +1,14 @@
 SimeBossMenu = {
     Server = {
         WebHook = {
-            ["Bonus"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["Promozione"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["Licenziamento"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["Assunzione"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["CambiaGrado"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["PrelievoDeposito"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["TogliTutteLeTransazioni"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["EliminaTransazione"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
+            ["Bonus"] = "",
+            ["Promozione"] = "",
+            ["Licenziamento"] = "",
+            ["Assunzione"] = "",
+            ["CambiaGrado"] = "",
+            ["PrelievoDeposito"] = "",
+            ["TogliTutteLeTransazioni"] = "",
+            ["EliminaTransazione"] = "",
         }
     }
 }
@@ -16,8 +16,8 @@ SimeBossMenu = {
 SimeMultiJob = {
     Server = {
         Webhook = {
-            ["CambioLavoro"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K",
-            ["RimozioneLavoro"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K"
+            ["CambioLavoro"] = "",
+            ["RimozioneLavoro"] = ""
         }
     }
 }
@@ -25,7 +25,8 @@ SimeMultiJob = {
 SimeFazioni = {
     Server = {
         Webhook = {
-            ["CreaFazione"] = "https://discord.com/api/webhooks/1300053842012143676/-x4XCbGtFL0ogQDYQ_FYJtNljwu4eXU-AMXLmVqtOIHs-oykpBnK9Ba8T0baGIkXTX-K"
+            ["CreaFazione"] = ""
         }
     }
+
 }
